5f9d9b6e1f4573b7129cce3566b4891c0b740919
Rune <sungjong5k@naver.com>
2025-03-18 02:54:05 +0900

    feat: update tailwind v4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <>
-      <h1>Hi there</h1>
+      <h1>Hi there!</h1>
       <Button>This is a button</Button>
     </>
   );
