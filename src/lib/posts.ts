5b04a2a90735712ba5114ad6d81a76ea7bf76e50
Rune <sungjong5k@naver.com>
2025-03-31 23:57:10 +0900

    fix: change redundant grouping router folder name

--- src/lib/posts.ts
+++ src/lib/posts.ts
@@ -2,14 +2,16 @@ import type { PostData } from "@/types";
 import { readdir } from "fs/promises";
 
 export async function getPosts(): Promise<PostData[]> {
-  const dirents = await readdir("./src/app/(posts)", { withFileTypes: true });
+  const dirents = await readdir("./src/app/(contents)", {
+    withFileTypes: true,
+  });
   const slugs = dirents
     .filter((dirent) => dirent.isDirectory())
     .map((dirent) => dirent.name);
 
   const posts = await Promise.all(
     slugs.map(async (slug) => {
-      const data = await import(`../app/(posts)/${slug}/page.mdx`);
+      const data = await import(`../app/(contents)/${slug}/page.mdx`);
       const metadata = data.meta;
       return { slug, ...metadata } as PostData;
     })
