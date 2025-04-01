499ced15029df3f9603b8fd8a5cac505c7824ca6
Rune <sungjong5k@naver.com>
2025-04-01 16:11:38 +0900

    fix: change getPosts logic

--- src/lib/posts.ts
+++ src/lib/posts.ts
@@ -1,25 +1,35 @@
-import type { PostData } from "@/types";
+import { PostData } from "@/types";
 import { readdir } from "fs/promises";
+import path from "path";
 
 export async function getPosts(): Promise<PostData[]> {
-  const dirents = await readdir("./src/app/(contents)", {
-    withFileTypes: true,
-  });
-  const slugs = dirents
-    .filter((dirent) => dirent.isDirectory())
-    .map((dirent) => dirent.name);
+  // MDX 파일들이 위치한 디렉토리
+  const postsDir = path.join(process.cwd(), "src", "app", "(contents)");
 
+  // 각 포스트 디렉토리 조회
+  const slugs = (await readdir(postsDir, { withFileTypes: true })).filter(
+    (dirent) => dirent.isDirectory()
+  );
+
+  // 각 MDX 파일에서 metadata를 추출한 후 PostData 형태로 매핑
   const posts = await Promise.all(
-    slugs.map(async (slug) => {
-      const data = await import(`../app/(contents)/${slug}/page.mdx`);
-      const metadata = data.meta;
-      return { slug, ...metadata } as PostData;
+    slugs.map(async (dirent) => {
+      const { meta } = await import(
+        `../app/(contents)/${dirent.name}/page.mdx`
+      );
+
+      return {
+        slug: dirent.name,
+        emoji: meta.emoji || "",
+        title: meta.title || "",
+        date: meta.date || meta.publishDate || "",
+        preview: meta.preview || "",
+        tag: meta.tag || "",
+      } as PostData;
     })
   );
 
-  posts.sort(
-    (a: PostData, b: PostData) => +new Date(b.date) - +new Date(a.date)
-  );
+  posts.sort((a, b) => +new Date(b.date) - +new Date(a.date));
 
   return posts;
 }
