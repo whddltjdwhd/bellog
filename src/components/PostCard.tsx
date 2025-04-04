b794ac2d98de719f2fb310d876074c9eefb158fa
Rune <sungjong5k@naver.com>
2025-04-04 14:54:51 +0900

    feat: edit interactive layout text size

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -12,27 +12,30 @@ export default function PostCard({ post }: PostCardProps) {
     <li>
       <Link
         href={`/${post.slug}`}
-        className="flex flex-col md:flex-row justify-between items-start md:items-center hover:bg-black/[.2] transition-all duration-100 rounded-2xl max-w-full p-4"
+        className="flex flex-col md:flex-row justify-between items-start md:items-center hover:bg-black/[.2] transition-all duration-100 rounded-2xl max-w-full p-3 md:p-4"
       >
-        <div className="flex items-center w-5/6">
+        <div className="flex items-center w-full md:w-5/6">
           {post.emoji && (
-            <span className="text-4xl md:text-5xl mr-4" aria-hidden="true">
+            <span
+              className="text-3xl md:text-4xl lg:text-5xl mr-2 md:mr-4"
+              aria-hidden="true"
+            >
               {post.emoji}
             </span>
           )}
 
           <div className="flex flex-col flex-1 min-w-0">
-            <h2 className="text-2xl md:text-3xl text-[#332C2C] font-bold truncate">
+            <h2 className="text-xl md:text-2xl lg:text-3xl text-[#332C2C] font-bold truncate">
               {post.title}
             </h2>
-            <span className="mt-1 text-base md:text-lg text-[#F6F4E2] truncate">
+            <span className="mt-1 text-sm md:text-base lg:text-lg text-[#F6F4E2] truncate">
               {post.preview}
             </span>
           </div>
         </div>
 
-        <div className="flex w-full md:w-auto flex-row md:flex-col items-start md:items-end mt-4 md:mt-0 gap-2 justify-between md:justify-start">
-          <p className="text-xs md:text-[11px] text-[#F6F4E2]">
+        <div className="flex w-full md:w-auto flex-row md:flex-col items-start md:items-end mt-2 md:mt-0 gap-1 md:gap-2 justify-between md:justify-start">
+          <p className="text-[10px] md:text-xs lg:text-[11px] text-[#F6F4E2]">
             {post.date.slice(2)}
           </p>
           <Tag tagName={post.tag} />
