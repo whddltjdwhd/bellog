597920d8d92e85967e99a82d050d8f890b1a15d3
Rune <sungjong5k@naver.com>
2025-04-24 01:45:12 +0900

    refactor: remove React.FC from MDX components

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -6,55 +6,56 @@ import CodeBlock from "./components/CodeBlock";
 import MDXImage from "./components/MDXImage";
 import MDXVideo from "./components/MDXVideo";
 
-const H1: React.FC<React.HTMLAttributes<HTMLHeadingElement>> = (props) => (
+const H1 = (props: React.HTMLAttributes<HTMLHeadingElement>) => (
   <h1
     className="text-[var(--text)] text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold my-7"
     {...props}
   />
 );
-const H2: React.FC<React.HTMLAttributes<HTMLHeadingElement>> = (props) => (
+
+const H2 = (props: React.HTMLAttributes<HTMLHeadingElement>) => (
   <h2
     className="text-[var(--text)] text-xl sm:text-2xl md:text-3xl lg:text-4xl font-semibold my-6"
     {...props}
   />
 );
-const H3: React.FC<React.HTMLAttributes<HTMLHeadingElement>> = (props) => (
+
+const H3 = (props: React.HTMLAttributes<HTMLHeadingElement>) => (
   <h3
     className="text-[var(--text)] text-lg sm:text-xl md:text-2xl lg:text-3xl font-semibold my-5"
     {...props}
   />
 );
-const H4: React.FC<React.HTMLAttributes<HTMLHeadingElement>> = (props) => (
+
+const H4 = (props: React.HTMLAttributes<HTMLHeadingElement>) => (
   <h4
     className="text-[var(--text)] text-base sm:text-lg md:text-xl lg:text-2xl font-semibold my-5"
     {...props}
   />
 );
-const H5: React.FC<React.HTMLAttributes<HTMLHeadingElement>> = (props) => (
+
+const H5 = (props: React.HTMLAttributes<HTMLHeadingElement>) => (
   <h5
     className="text-[var(--text)] text-sm sm:text-base md:text-lg lg:text-xl font-semibold my-4"
     {...props}
   />
 );
-const H6: React.FC<React.HTMLAttributes<HTMLHeadingElement>> = (props) => (
+
+const H6 = (props: React.HTMLAttributes<HTMLHeadingElement>) => (
   <h6
     className="text-[var(--text)] text-xs sm:text-sm md:text-base lg:text-lg font-semibold my-4"
     {...props}
   />
 );
 
-const Paragraph: React.FC<React.HTMLAttributes<HTMLParagraphElement>> = (
-  props
-) => (
+const Paragraph = (props: React.HTMLAttributes<HTMLParagraphElement>) => (
   <p
     className="text-[var(--text)] text-sm sm:text-base md:text-lg my-4 leading-8"
     {...props}
   />
 );
 
-const Link: React.FC<React.AnchorHTMLAttributes<HTMLAnchorElement>> = (
-  props
-) => (
+const Link = (props: React.AnchorHTMLAttributes<HTMLAnchorElement>) => (
   <a
     className="text-[var(--link)] hover:underline"
     target="_blank"
@@ -63,61 +64,55 @@ const Link: React.FC<React.AnchorHTMLAttributes<HTMLAnchorElement>> = (
   />
 );
 
-const Blockquote: React.FC<React.BlockquoteHTMLAttributes<HTMLElement>> = (
-  props
-) => (
+const Blockquote = (props: React.BlockquoteHTMLAttributes<HTMLElement>) => (
   <blockquote
     className="border-l-4 border-[var(--border)] pl-4 italic my-4 text-[var(--accent)] bg-[var(--surface)]"
     {...props}
   />
 );
 
-const InlineCode: React.FC<React.HTMLAttributes<HTMLElement>> = (props) => (
+const InlineCode = (props: React.HTMLAttributes<HTMLElement>) => (
   <code
     className="bg-[var(--code)] px-1 py-0.5 rounded font-mono text-xs sm:text-sm md:text-base"
     {...props}
   />
 );
 
-const UnorderedList: React.FC<React.HTMLAttributes<HTMLUListElement>> = (
-  props
-) => <ul className="text-[var(--text)] list-disc pl-6 my-2" {...props} />;
+const UnorderedList = (props: React.HTMLAttributes<HTMLUListElement>) => (
+  <ul className="text-[var(--text)] list-disc pl-6 my-2" {...props} />
+);
 
-const OrderedList: React.FC<React.OlHTMLAttributes<HTMLOListElement>> = (
-  props
-) => <ol className="text-[var(--text)] list-decimal pl-6 my-2" {...props} />;
+const OrderedList = (props: React.OlHTMLAttributes<HTMLOListElement>) => (
+  <ol className="text-[var(--text)] list-decimal pl-6 my-2" {...props} />
+);
 
-const ListItem: React.FC<React.HTMLAttributes<HTMLLIElement>> = (props) => (
+const ListItem = (props: React.HTMLAttributes<HTMLLIElement>) => (
   <li className="my-1" {...props} />
 );
 
-const Hr: React.FC<React.HTMLAttributes<HTMLHRElement>> = (props) => (
+const Hr = (props: React.HTMLAttributes<HTMLHRElement>) => (
   <hr className="mb-3 border-t border-[var(--border)]" {...props} />
 );
 
-const TableRow: React.FC<React.HTMLAttributes<HTMLTableRowElement>> = (
-  props
-) => <tr className="border-b border-[var(--border)]" {...props} />;
+const TableRow = (props: React.HTMLAttributes<HTMLTableRowElement>) => (
+  <tr className="border-b border-[var(--border)]" {...props} />
+);
 
-const TableData: React.FC<React.TdHTMLAttributes<HTMLTableCellElement>> = (
-  props
-) => (
+const TableData = (props: React.TdHTMLAttributes<HTMLTableCellElement>) => (
   <td
     className="px-4 py-2 border border-[var(--border)] text-center"
     {...props}
   />
 );
 
-const TableHeader: React.FC<React.ThHTMLAttributes<HTMLTableCellElement>> = (
-  props
-) => (
+const TableHeader = (props: React.ThHTMLAttributes<HTMLTableCellElement>) => (
   <th
     className="px-10 py-2 font-semibold text-left border border-[var(--border)]"
     {...props}
   />
 );
 
-const LineBreak: React.FC<React.HTMLAttributes<HTMLBRElement>> = (props) => (
+const LineBreak = (props: React.HTMLAttributes<HTMLBRElement>) => (
   <br className="m-96" {...props} />
 );
 
