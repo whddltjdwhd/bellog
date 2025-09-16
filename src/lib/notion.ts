d2b5b5216418228fe3abc3e69a3a53378c4a5df9
Rune <sungjong5k@naver.com>
2025-09-16 21:13:19 +0900

    feat: add request hook to modify syncRecordValues API

--- src/lib/notion.ts
+++ src/lib/notion.ts
@@ -13,6 +13,27 @@ const notion = new Client({
 const notionX = new NotionAPI({
   activeUser: process.env.NOTION_ACTIVE_USER,
   authToken: process.env.NOTION_TOKEN_V2,
+  kyOptions: {
+    hooks: {
+      beforeRequest: [
+        (request, options) => {
+          const url = request.url.toString();
+
+          if (url.includes("/api/v3/syncRecordValues")) {
+            return new Request(
+              url.replace(
+                "/api/v3/syncRecordValues",
+                "/api/v3/syncRecordValuesMain"
+              ),
+              options
+            );
+          }
+
+          return request;
+        },
+      ],
+    },
+  },
 });
 
 export const getAllPostsFromNotion = async (): Promise<Post[]> => {
