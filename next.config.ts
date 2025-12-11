73df027385ed6050796852779da96bea839c3ca2
Rune <sungjong5k@naver.com>
2025-12-11 21:50:28 +0900

    chore: add notion related url

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,14 @@ const nextConfig = {
       { protocol: "https", hostname: "abs.twimg.com" },
       { protocol: "https", hostname: "pbs.twimg.com" },
       { protocol: "https", hostname: "s3.us-west-2.amazonaws.com" },
+      { 
+        protocol: "https", 
+        hostname: "prod-files-secure.s3.us-west-2.amazonaws.com" 
+      },
+      { 
+        protocol: "https", 
+        hostname: "file.notion.so" 
+      },
     ],
     formats: ["image/avif", "image/webp"],
     dangerouslyAllowSVG: true,
